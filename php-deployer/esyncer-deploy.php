0f51c06fdd2cb7871ac510059ad44a81ede1357e
EnplatedSyncer <karel4cermak@seznam.cz>
2022-07-13 02:34:18 +0200

    Added one .gitKeep and deleted error log in PHP deployer

--- php-deployer/esyncer-deploy.php
+++ php-deployer/esyncer-deploy.php
@@ -1,7 +1,4 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-
     header('Content-Type: text/html; charset=utf-8');
     session_start();
     require_once "esyncer-vars.php";
